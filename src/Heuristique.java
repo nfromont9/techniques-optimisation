81f960e81b07dc89f954f9e1b155c8b83128a2bf
gab <gabriel.ley@outlook.fr>
2018-03-22 18:55:12 +0100

    Ajout de calcul de combinaisons élémentaires

--- src/Heuristique.java
+++ src/Heuristique.java
@@ -45,6 +45,103 @@ public class Heuristique {
     public static void main(String[] args) {
         getArgs(args);
         print();
+        List<ArrayList<Integer>> combiElem = getCombiElem();
+    }
 
+    private static List<ArrayList<Integer>> getCombiElem(){
+        //On recupere les capteurs par zone
+        ArrayList<Integer> capteursParZone[] = getCapteurParZone();
+
+        ArrayList<ArrayList<Integer>> allConfigs = new ArrayList<>();
+
+        //nombre d'iteration limite
+        for(int iter = 0; iter < nbC+nbZ; iter++){
+
+            ArrayList<Integer> config = new ArrayList<>();
+            //
+            for(ArrayList<Integer> zone : capteursParZone){
+                boolean toAdd = true;
+                for(int capteur : zone){
+                    //On verifie si la config contient deja un capteur de la zone
+                    if(config.contains(capteur)){
+                        toAdd = false;
+                    }
+                }
+                if(toAdd){
+                    //Si il n'y a pas de capteur de la zone
+                    config.add(zone.get((int)(Math.random()*zone.size())));
+
+                }
+            }
+
+            boolean ok = true;
+            //On enleve les capteurs inutiles de la configuration
+            config = elementarise(config, zones);
+
+            //Si on a pas encore trouver cette combinaison on l'ajoute a la liste de retour
+            for(ArrayList x : allConfigs){
+                if(configEgale(config, x)) ok = false;
+            }
+            if(ok){
+                allConfigs.add(config);
+            }
+        }
+        return allConfigs;
+    }
+
+    private static boolean configEgale(ArrayList<Integer> config, ArrayList x) {
+        if(config.size() != x.size()) return false;
+
+        for(int i : config){
+            if(!x.contains(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    //On regarde les zones couvertes de chaque capteur de la configuration et on les met dans une liste
+    //Si une zone est couverte par un autre capteur de la configuration on l'enleve de la liste
+    //Si au bout du compte cette liste est vide le capteur est inutile et enlever de la configuration
+    private static ArrayList<Integer> elementarise(ArrayList<Integer> config, List<int[]> zones) {
+        int idx = 0;
+        while(idx < config.size()){
+            ArrayList<Integer> current = new ArrayList<>();
+            for(int i : zones.get(config.get(idx))) current.add(i);
+            for(int capteur : config){
+                if(capteur == config.get(idx)){
+                    continue;
+                }
+                int i = 0;
+                while(i < current.size()){
+                    if(Arrays.binarySearch(zones.get(capteur), current.get(i)) >= 0){
+                        current.remove(i);
+                    }
+                    else i++;
+                }
+            }
+            if(current.size() == 0) config.remove(idx);
+            else idx ++;
+        }
+        return config;
+    }
+
+    private static ArrayList<Integer>[] getCapteurParZone(){
+        ArrayList<Integer> capteursParZone[] = new ArrayList[nbZ];
+
+        for(int i = 0; i < capteursParZone.length; i++){
+            capteursParZone[i] = new ArrayList<Integer>();
+        }
+
+        for(int[] zonesParCapteur : zones){
+            for(int zone : zonesParCapteur){
+                if(!capteursParZone[zone-1].contains(zones.indexOf(zonesParCapteur))){
+                    capteursParZone[zone-1].add(zones.indexOf(zonesParCapteur));
+                }
+            }
+        }
+
+        return capteursParZone;
     }
 }
